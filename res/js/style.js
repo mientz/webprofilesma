06c16e2ae95d29f2395acdb2150c4ed316fa5127
mientz <genthowijaya@gmail.com>
2016-10-07 23:54:06 +0700

    header & footer

---
+++ res/js/style.js
@@ -0,0 +1,7 @@
+//    if ($(window).width() >= 1200){
+//        $('ul.nav li.dropdown').hover(function() {
+//            $(this).find('.dropdown-menu').stop(true, true).delay(50).fadeIn(50);
+//        }, function() {
+//            $(this).find('.dropdown-menu').stop(true, true).delay(50).fadeOut(50);
+//        });
+//    }
